8222bb78b03cb875f6c108ef15f676902b1c1adb
WadekarNikhil <94883743+WadekarNikhil@users.noreply.github.com>
2021-12-13 19:31:41 +0530

    Update Address_Book.py

--- Address_Book.py
+++ Address_Book.py
@@ -1,102 +1,95 @@
-# import library
+#import library
 from tkinter import *
 
-# initialize window
+#initialize window
 root = Tk()
-root.geometry('600x600')
-bg = PhotoImage(file="C:/vc/img11.png")
-my_label = Label(root, image=bg)
-my_label.place(x=0, y=0, relwidth=1, relheight=1)
-root.title('VC Book')
-root.resizable(0, 0)
+root.geometry('400x400')
+root.config(bg = 'SlateGray3')
+root.title('DataFlair-AddressBook')
+root.resizable(0,0)
 contactlist = [
-    ['Sample Contact', 00000000, 'mail']
-]
+    ['Parv Maheswari',  '0176738493'],
+    ['David Sharma',  '2684430000'],
+    ['Mandish Kabra',   '4338354432'],
+    ['Prisha Modi','6834552341'],
+    ['Rahul kaushik',   '1234852689'],
+    ['Johena Shaa' , '2119876543'],
+    ]
 
 Name = StringVar()
 Number = StringVar()
-Email = StringVar()
 
-# create frame
+
+#create frame
 frame = Frame(root)
-frame.pack(side=LEFT)
+frame.pack(side = RIGHT)
 
-scroll = Scrollbar(frame, orient=HORIZONTAL)
-select = Listbox(frame, yscrollcommand=scroll.set, height=20, width=30)
-scroll.config(command=select.yview)
-scroll.pack(side=LEFT, fill=Y)
-select.pack(side=LEFT, fill=BOTH, expand=1)
+scroll = Scrollbar(frame, orient=VERTICAL)
+select = Listbox(frame, yscrollcommand=scroll.set, height=12)
+scroll.config (command=select.yview)
+scroll.pack(side=RIGHT, fill=Y)
+select.pack(side=LEFT,  fill=BOTH, expand=1)
 
 
-# function to get select value
+########### function to get select value
 
 def Selected():
     return int(select.curselection()[0])
-
-
-# to add a new contact
+    
+##fun to add new contact
 def AddContact():
-    contactlist.append([Name.get(), Number.get(), Email.get()])
+    contactlist.append([Name.get(), Number.get()])
     Select_set()
-
 
 # fun to edit existing contact(first select the contact then click on view button then edit the contact and then click on edit button)
 def EDIT():
-    contactlist[Selected()] = [Name.get(), Number.get(),Email.get()]
+    contactlist[Selected()] = [Name.get(), Number.get()]
     Select_set()
-
-
-# to delete selected contact
+    
+#to delete selected contact
 def DELETE():
     del contactlist[Selected()]
     Select_set()
-
-
+   
 # to view selected contact(first select then click on view button)
 def VIEW():
-    NAME, PHONE, mail = contactlist[Selected()]
+    NAME, PHONE = contactlist[Selected()]
     Name.set(NAME)
     Number.set(PHONE)
-    Email.set(mail)
-    Select_set()
 
 
-#exit  window
+###exit game window   
 def EXIT():
     root.destroy()
 
-
-# empty name and number field
+#empty name and number field
 def RESET():
     Name.set('')
     Number.set('')
-    Email.set('')
 
 
-def Select_set():
+def Select_set() :
     contactlist.sort()
-    select.delete(0, END)
-    for name, phone, Email in contactlist:
-        select.insert(END, name)
-
-
+    select.delete(0,END)
+    for name,phone in contactlist :
+        select.insert (END, name)
 Select_set()
- #labels and entry widget
-Label(root, text="VC CONTACT BOOK", font='Bold', bg='Orange').place(x=225, y=65)
 
-Label(root, text='Name', font='italic', bg='WHITE').place(x=330, y=180)
-Entry(root, textvariable=Name).place(x=420, y=185)
-Label(root, text='Mobile nO.', font='italic', bg='WHITE').place(x=330, y=213)
-Entry(root, textvariable=Number).place(x=420, y=215)
-Label(root, text='Email', font='italic', bg='WHITE').place(x=330, y=255)
-Entry(root, textvariable=Email).place(x=420, y=255)
-#define buttons
-Button(root, text=" Add", font='arial 12 bold', bg='SlateGray4', command=AddContact).place(x=30, y=550)
-Button(root, text="Update", font='arial 12 bold', bg='SlateGray4', command=EDIT).place(x=110, y=550)
-Button(root, text="Delete", font='arial 12 bold', bg='SlateGray4', command=DELETE).place(x=200, y=550)
-Button(root, text="View", font='arial 12 bold', bg='SlateGray4', command=VIEW).place(x=300, y=550)
-Button(root, text="EXIT", font='arial 12 bold', bg='RED', command=EXIT).place(x=400, y=550)
-Button(root, text="Reset", font='arial 12 bold', bg='SlateGray4', command=RESET).place(x=500, y=550)
+
+
+######define buttons #####labels and entry widget
+Label(root, text = 'NAME', font='arial 12 bold', bg = 'SlateGray3').place(x= 30, y=20)
+Entry(root, textvariable = Name).place(x= 100, y=20)
+Label(root, text = 'PHONE NO.', font='arial 12 bold',bg = 'SlateGray3').place(x= 30, y=70)
+Entry(root, textvariable = Number).place(x= 130, y=70)
+
+Button(root,text=" ADD", font='arial 12 bold',bg='SlateGray4', command = AddContact).place(x= 50, y=110)
+Button(root,text="EDIT", font='arial 12 bold',bg='SlateGray4',command = EDIT).place(x= 50, y=260)
+Button(root,text="DELETE", font='arial 12 bold',bg='SlateGray4',command = DELETE).place(x= 50, y=210)
+Button(root,text="VIEW", font='arial 12 bold',bg='SlateGray4', command = VIEW).place(x= 50, y=160)
+Button(root,text="EXIT", font='arial 12 bold',bg='tomato', command = EXIT).place(x= 300, y=320)
+Button(root,text="RESET", font='arial 12 bold',bg='SlateGray4', command = RESET).place(x= 50, y=310)
+
 
 root.mainloop()
-
+  
